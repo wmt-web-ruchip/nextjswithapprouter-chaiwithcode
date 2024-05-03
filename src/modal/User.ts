f75c6d569b367030970c63d51d7923c2cf4e0cab
ruchipatel40 <ruchip@webmob.tech>
2024-05-03 16:25:15 +0530

    first commit

---
+++ src/modal/User.ts
@@ -0,0 +1,71 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  isVerified: boolean;
+  verifyCodeExpiry: Date;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<Message> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required."],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    unique: true,
+    match: [/.+\@.+\,.+/, "Please use a valid email address."],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required."],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify code is required."],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code expiry is required."],
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+
+const UserModal =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModal;
